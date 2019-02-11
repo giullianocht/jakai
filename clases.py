97917c0cdf8a0d5c174b7f81c8a0b9d50026b330
Giulliano Albrecht <mateogiulliano@hotmail.com>
2019-02-11 10:48:08 -0300

    1.0

--- clases.py
+++ clases.py
@@ -1,1 +1,12 @@
 #En proceso
+class Robot:
+    def __init__(self,un_nombre=""):
+        self.nombre = un_nombre
+    def adelante(self):
+        #En breve
+    def reversa(self):
+        #En breve
+    def izq(self):
+        #En breve
+    def der(self):
+        #En breve
