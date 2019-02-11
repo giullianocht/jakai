34f769c767e437f8ee253186f5a92c0f5be0a4c6
Giulliano Albrecht <mateogiulliano@hotmail.com>
2019-02-11 11:56:07 -0300

    1.2

--- clases.py
+++ clases.py
@@ -1,16 +1,33 @@
 #En proceso
 class Robot:
-    def __init__(self,un_nombre=""):
+
+    def __init__(self,un_nombre ="",der1,izq1,der2,izq2):
         self.nombre = un_nombre
+
     def avanzar(self):
-        #En breve
-        gpio.output(motor_der1,True)
-        gpio.output(motor_izq1,True)
-        gpio.output(motor_der1,False)
-        gpio.output(motor_izq1,False)
+
+        gpio.output(der1,True)
+        gpio.output(izq1,True)
+        gpio.output(der2,False)
+        gpio.output(izq2,False)
+
     def reversa(self):
-        #En breve
+
+        gpio.output(der1,False)
+        gpio.output(izq1,False)
+        gpio.output(der2,True)
+        gpio.output(izq2,True)
+
     def izq(self):
-        #En breve
+
+        gpio.output(der1,True)
+        gpio.output(izq1,False)
+        gpio.output(der2,False)
+        gpio.output(izq2,True)
+
     def der(self):
         #En breve
+        gpio.output(der1,False)
+        gpio.output(izq1,True)
+        gpio.output(der2,True)
+        gpio.output(izq2,False)
--- raps.py
+++ raps.py
@@ -10,4 +10,7 @@ motor_izq2 = 21     					 #definimos los pines de GPIO a utilizar
 
 gpio.setmode(gpio.BCM) 			# modo BCM de la raspberry pi (Broadcom SOC channel)
 
-gpio.setup(led1,gpio.OUT)       #co
+gpio.setup(motor_der1,gpio.OUT) # Se configura el motor Der 1
+gpio.setup(motor_der2,gpio.OUT) # Se configura el motor Der 2
+gpio.setup(motor_izq1,gpio.OUT) # Se configura el motor Izq 1
+gpio.setup(motor_izq2,gpio.OUT) # Se configura el motor Izq 2
