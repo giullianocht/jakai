fa27926c88759d71525fd4ae73648ec94c75696b
Giulliano Albrecht <mateogiulliano@hotmail.com>
2019-02-12 11:46:53 -0300

    Si no tiene control te avisa 2.1

--- ps3_controller.py
+++ ps3_controller.py
@@ -3,54 +3,51 @@ import time
 
 pygame.init()
 pygame.joystick.init()
+if not pygame.joystick.get_init() == 0 :
 
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
 
-#print 'Initialised Joystick : %s' % joystick.get_name()
-#print 'Press ESC to stop'
+    joystick_count = pygame.joystick.get_count()
+    """print("joystick_count")
+    print(joystick_count)
+    print("--------------")"""
 
-#screen = pygame.display.set_mode((100,100))
+    numaxes = joystick.get_numaxes()
+    """print("numaxes")
+    print(numaxes)
+    print("--------------")"""
+    numbuttons = joystick.get_numbuttons()
+    """print("numbuttons")
+    print(numbuttons)
+    print("--------------")"""
+    loopQuit = False
+    while loopQuit == False:
+        time.sleep(0.5)
+    # test joystick axes
+        for i in range(0,4):
+            pygame.event.pump()
+            axis = joystick.get_axis(i)
+            if i == 0 and axis < 0:
+                print("Izquierda")
+            if i == 0 and axis > 0:
+                print("Derecha")
+        #test controller buttons
+        for i in range(0,numbuttons):
+            pygame.event.pump()
+            button = joystick.get_button(i)
+            if i == 9 and button == 1:
+                loopQuit = True
+            if i == 0 and button == 1:
+                print ("Apretaste X")
+            if i == 1 and button == 1:
+                print ("Apretaste Circulo")
+            if i == 2 and button == 1:
+                print ("Apretaste Triangulo")
+            if i == 3 and button == 1:
+                print ("Apretaste Cuadrado")
 
-# get count of joysticks=1, axes=27, buttons=19 for DualShock 3
+    #pygame.quit()
 
-joystick_count = pygame.joystick.get_count()
-print("joystick_count")
-print(joystick_count)
-print("--------------")
-
-numaxes = joystick.get_numaxes()
-print("numaxes")
-print(numaxes)
-print("--------------")
-
-numbuttons = joystick.get_numbuttons()
-print("numbuttons")
-print(numbuttons)
-print("--------------")
-loopQuit = False
-while loopQuit == False:
-    time.sleep(0.5)
-# test joystick axes
-    for i in range(0,4):
-        pygame.event.pump()
-        axis = joystick.get_axis(i)
-        print("Axis " + str(i) + " = " + str(axis))
-    print("--------------")
-
-    #test controller buttons
-    for i in range(0,numbuttons):
-        pygame.event.pump()
-        button = joystick.get_button(i)
-        if i == 9 and button == 1:
-            loopQuit = True
-        if i == 0 and button == 1:
-            print ("Apretaste X")
-        if i == 1 and button == 1:
-            print ("Apretaste Circulo")
-        if i == 2 and button == 1:
-            print ("Apretaste Triangulo")
-        if i == 3 and button == 1:
-            print ("Apretaste Cuadrado")
-
-pygame.quit()
+else :
+    print("Conecte un control y vuelva a intentarlo")
