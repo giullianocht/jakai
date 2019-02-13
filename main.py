36006d57960bf8335e5764f8d7908b1fc8c01ad2
Giulliano Albrecht <mateogiulliano@hotmail.com>
2019-02-13 15:47:13 -0300

    Version comentada y prepara para la presentación

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 #_*_ coding: utf-8 _*_
 """
-     JAKAI - Proyecto Hackathon BootCamp Penguin Academy 2019
-     Asunción - Paraguay
+            JAKAI - Proyecto Hackathon BootCamp Penguin Academy 2019
+
+                        Asunción - Paraguay
+
+                Proyecto de codigo abierto con license GNU
+
+            --------> https://github.com/albrecht99/Jakai <--------
+
      Mentor : Williams Bobadilla
+
      Integrantes
-     1) Giulliano Albrecht Instagram --> giulliano_cht
-     2) Miguel Castiñeira Instagram --> miguel_castinheira
-     3) Aldo Franco Instagram --> Aldo.franco1
-     4) Ivan Ayala  Instagram --> ivanrayala
+
+     1) Giulliano Albrecht --> Instagram --> giulliano_cht
+     2) Miguel Castiñeira  --> Instagram --> miguel_castinheira
+     3) Aldo Franco  --> Instagram --> Aldo.franco1
+     4) Ivan Ayala   --> Instagram --> ivanrayala
 
 """
 import RPi.GPIO as gpio      # libreria para utilizar los puertos de entrada y salida
-import pygame
+import pygame                # libreira para utilizar el control de PS3
 import time
+
+# Se define los puerto de la raspberry
 
 motor_der1 = 18
 motor_der2 = 19
@@ -27,9 +37,13 @@ gpio.setup(motor_der2,gpio.OUT) # Se configura el motor Der 2
 gpio.setup(motor_izq1,gpio.OUT) # Se configura el motor Izq 1
 gpio.setup(motor_izq2,gpio.OUT) # Se configura el motor Izq 2
 
+
+
+# Se crea la clase Robot
+
 class Robot:
 
-    def __init__(self,un_nombre,der1,izq1,der2,izq2):
+    def __init__(self,un_nombre,der1,izq1,der2,izq2): #La clase recibe un nombre y los motores
         self.nombre = un_nombre
         self.der1 = der1
         self.izq1 = izq1
@@ -61,36 +75,40 @@ class Robot:
         gpio.output(self.der2,False)
         gpio.output(self.izq2,False)
 
+# Se inicializa JAKAI y se le pasa los motores a utilizar
+
 JAKAI = Robot("Jakai",motor_der1,motor_izq1,motor_der2,motor_izq2)
 
+# Se inicializa la libreria PYGAME y se inicia la busqueda de un Joystick en nuestro caso
+# un control de play station 3
 pygame.init()
 pygame.joystick.init()
 
 
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
+joystick = pygame.joystick.Joystick(0) # Se carga el control encontrado en la variable joystick
+joystick.init() # se inicializa el joystick ya cargado
 
-joystick_count = pygame.joystick.get_count()
+joystick_count = pygame.joystick.get_count()# la variable joystick_count guarda la cantidad de joystick que tenemos
 """print("joystick_count")
 print(joystick_count)
 print("--------------")"""
 
-numaxes = joystick.get_numaxes()
+numaxes = joystick.get_numaxes() # numaxes guarda la cantidad de ejes "Axes" de nuestro control
 """print("numaxes")
 print(numaxes)
 print("--------------")"""
-numbuttons = joystick.get_numbuttons()
+numbuttons = joystick.get_numbuttons() # numbuttons guarda la cantidad de botones de nuestro control
 """print("numbuttons")
 print(numbuttons)
 print("--------------")"""
 
 loopQuit = False
-while loopQuit == False:
+while loopQuit == False: # Se inicializa un ciclo con time sleep de 0.1
     time.sleep(0.1)
     # test joystick axes
     for i in range(0,4):
         pygame.event.pump()
-        axis = joystick.get_axis(i)
+        axis = joystick.get_axis(i) # axis recibe si los ejes se estan moviendo
         if i == 0 and axis < 0:
             JAKAI.izq()
             #print("Izquierda")
@@ -100,25 +118,20 @@ while loopQuit == False:
         #test controller buttons
     for i in range(0,numbuttons):
         pygame.event.pump()
-        button = joystick.get_button(i)
-        if i == 9 and button == 1:
+        button = joystick.get_button(i) #button recibe si se esta presionando o no un boton
+        if i == 9 and button == 1: # Si se presiona el boton 9 "start" se termina el ciclo
             loopQuit = True
-        if i == 8 and button == 1:
+        if i == 8 and button == 1:  #Si se presiona el boton 8 "select" los 4 motores se detienen
             JAKAI.detener()
-            # Si se presiona select se detiene
-        if i == 7 and button == 1:
-            #print ("Apretaste R2")
+        if i == 7 and button == 1: #Si se presiona el boton 7 "R2" se avanza
             JAKAI.avanzar()
-        if i == 6 and button == 1:
+        if i == 6 and button == 1: #Si se presiona el boton 6 "L2" se pone en reversa
             #print ("Apretaste L2")
             JAKAI.reversa()
-        """if i == 0 and button == 1:
-            #print ("Apretaste X")
-        if i == 1 and button == 1:
-            #print ("Apretaste Circulo")
-        if i == 2 and button == 1:
-            #print ("Apretaste Triangulo")
-        if i == 3 and button == 1:
-            #print ("Apretaste Cuadrado")"""
-
+#Al salir del ciclo se finaliza la libreia PYGAME
 pygame.quit()
+
+
+
+
+                                "MUCHAS GRACIAS"
